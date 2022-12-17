dce53d9f4c1f7795c23e16d140fa8506b5f2fc4f
jorgeMendes-escala <jorge.mendes@escala-app.com>
2022-12-17 16:07:27 -0300

    SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,13 +19,33 @@ class MyDocument extends Document {
           <meta property='og:type' content='website' />
           <link rel='icon' href='/favicon.ico' />
 
-          <meta name="keywords" content="Taróloga, Oraculista, Afrodite, Afrodite Cartomante, Cartomante, Baralho Cigano, Maria Padilha, Axé, Tarot, Afrodite Taróloga, A melhor taróloga do Brasil" />
+          <meta 
+            name="keywords" 
+            content="
+              Taróloga, 
+              Oraculista, 
+              Afrodite, 
+              Afrodite Cartomante, 
+              Cartomante, 
+              Baralho Cigano, 
+              Maria Padilha, 
+              Axé, 
+              Tarot, 
+              Afrodite Taróloga, 
+              A melhor Taróloga do Brasil,
+              Zé Pelintra,
+              Tarot Online,
+              Magias" />
           <meta name="author" content="Afrodite Cartomante" />
 
           <meta name="theme-color" content="#CB6CE7" /> 
           <meta name="apple-mobile-web-app-status-bar-style" content="#CB6CE7" /> 
           <meta name="msapplication-navbutton-color" content="#CB6CE7" /> 
 
+          <meta property="og:image" content="https://afroditecartomante.netlify.app/assets/images/bg02.jpeg" />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="800" /> 
+          <meta property="og:image:height" content="600" />
         </Head>
         <body>
           <Main />
